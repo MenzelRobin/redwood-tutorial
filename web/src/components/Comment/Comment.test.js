b7080c2b29353e1111d5c64873a7cf84a83232ff
MenzelRobin <robin.menzel@ymail.com>
2023-01-05 21:52:17 +0100

    Chapter 6 Building a component completed

---
+++ web/src/components/Comment/Comment.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@redwoodjs/testing/web'
+
+import Comment from './Comment'
+
+describe('Comment', () => {
+  it('renders successfully', () => {
+    const comment = {
+      name: 'John Doe',
+      body: 'This is my comment',
+      createdAt: '2020-01-02T12:34:56Z',
+    }
+    render(<Comment comment={comment} />)
+
+    expect(screen.getByText(comment.name)).toBeInTheDocument()
+    expect(screen.getByText(comment.body)).toBeInTheDocument()
+  })
+})
